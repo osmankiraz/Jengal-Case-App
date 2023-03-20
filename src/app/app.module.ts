9739ebdb2b13dc9c293a7e8a6d9401ccc682ea9d
Osman Kiraz <45825918+osmankiraz@users.noreply.github.com>
2023-03-20 20:53:27 +0300

    updated app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { IconComponent } from './components/icon/icon.component';
+import { ListComponent } from './components/list/list.component';
+import { EntryComponent } from './components/entry/entry.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, IconComponent, ListComponent, EntryComponent, MainComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
+  exports:[IconComponent]
 })
 export class AppModule {}
