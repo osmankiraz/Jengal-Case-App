06b6e6b60e4f5e3ee74cac1c304a27b6c1e89a88
Osman Kiraz <45825918+osmankiraz@users.noreply.github.com>
2023-03-20 20:54:02 +0300

    Created icon types

---
+++ src/app/types/icon.type.ts
@@ -0,0 +1,11 @@
+/**
+ * Iconların boyut değerlerini saklayan Type'tır
+ */
+export type IconSizeType = '' | 'fa-lg' | 'fa-2x' | 'fa-3x' | 'fa-4x' | 'fa-5x';
+
+/**
+ * Icon isimlerinin başındaki prefix değerlerini saklayan Type'tır.
+ * Kullandığımız font awesome sürümü version 4 tür ve version 4 te genellikle
+ * bu prefix değeri 'fa fa-' dir
+ */
+export type IconPrefixType = 'fa fa-' | 'fa fa-cc-' | 'fal fa-' | 'fas fa-';
