e6de1e47caab7383f2dae43fc32b5b13a1ee27a6
Osman Kiraz <45825918+osmankiraz@users.noreply.github.com>
2023-03-20 20:51:57 +0300

    created entry.component

---
+++ src/app/components/entry/entry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Jengal-entry',
+  templateUrl: './entry.component.html',
+  styleUrls: ['./entry.component.scss']
+})
+export class EntryComponent {
+
+}
